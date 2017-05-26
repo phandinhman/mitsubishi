4e9043a1b8a9c1387017d58baecf8417aa3ae426
Phan Dinh Man <phandinhman@gmail.com>
2017-05-26 17:11:56 +0700

    Fix CORS

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,12 @@ module Mitsubishi
     end
 
     config.autoload_paths << Rails.root.join("lib")
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :patch, :options, :head]
+      end
+    end
   end
 end
