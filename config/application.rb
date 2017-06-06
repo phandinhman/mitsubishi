a2839688df48f4a59f612cbabc0b40b5129fd260
Phan Dinh Man <phandinhman@gmail.com>
2017-06-06 10:54:00 +0700

    Fix auto load library

--- config/application.rb
+++ config/application.rb
@@ -15,14 +15,15 @@ module Mitsubishi
 
     config.active_record.raise_in_transactional_callbacks = true
 
+    config.autoload_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("lib")
+
     config.i18n.load_path += Dir["#{Rails.root.to_s}/config/locales/**/*.{rb,yml}"]
     config.autoload_paths += Dir["#{config.root}/app/models/**/"]
 
     Dir.glob("config/routes/*").each do |route|
       config.paths["config/routes.rb"] << Rails.root.join(route)
     end
-
-    config.autoload_paths << Rails.root.join("lib")
 
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
